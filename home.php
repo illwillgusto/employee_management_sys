c92c2c02678214488f02913d6b713c2a0d424ffe
Will Walton <will@endless3dge.com>
2024-04-27 12:52:35 -0500

    Update home.php
    html script with description updated added

--- home.php
+++ home.php
@@ -9,3 +9,15 @@ $dbname = $database_name;
 
 $dbcon = $connection_object->connection('localhost', $dbname, $dbuser, $dbpass);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Employee Management Dashboard</title>
+</head>
+<body>
+  
+</body>
+</html>
