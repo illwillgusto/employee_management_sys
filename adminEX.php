33998509dcff43fbbf5c06c86cb5b62d3a3bce77
Will Walton <will@endless3dge.com>
2024-04-28 09:28:04 -0500

    Update adminEX.php
    html script added
    
    font awesome cdn added to header
    jquery v3 cdn added to header
    bootdtrap v5 cdn added to header

--- adminEX.php
+++ adminEX.php
@@ -16,3 +16,26 @@ else {
   }
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>AdminEX</title>
+  <link rel="stylesheet" type="text/css" href="myProfile.css">
+
+  <!-- Font Awesome -->
+  <script src="https://kit.fontawesome.com/8716b95af6.js" crossorigin="anonymous"></script>
+
+  <!-- JQuery Library -->
+  <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+
+<!-- Bootstrap -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+
+</head>
+<body>
+
+</body>
+</html>
