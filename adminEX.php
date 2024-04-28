3d150f2f4a8c136d278d4077970f7743886fa5eb
Will Walton <will@endless3dge.com>
2024-04-28 09:06:52 -0500

    Update adminEX.php
    else statement added if a user attempts to access the admin portion of the system

--- adminEX.php
+++ adminEX.php
@@ -5,4 +5,11 @@ if ($_SESSION['login'] != "True") {
   $error= "Sorry You Have To Login First.";
   echo("<script>alert(\"$error\")</script>");
 }
+
+else {
+  if ($_SESSION['user_Role'] != "Admin") {
+    $error="Sorry You Cannot View This Page.";
+    echo("<script>alert(\"$error\")</script>");
+  }
+}
 ?>
