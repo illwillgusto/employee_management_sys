83993b5e5215850d257c91e14f4a83790cc4557d
Will Walton <will@endless3dge.com>
2024-04-30 18:16:54 -0500

    Update home.php
    navbar container added
    contaienr-fluid div added
    navbar div tag added
    button element added

--- home.php
+++ home.php
@@ -27,5 +27,18 @@ $dbcon = $connection_object->connection('localhost', $dbname, $dbuser, $dbpass);
 </head>
 <body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="50">
 
+<!-- Navbar start -->
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+  </div>
+</nav>
+
 </body>
 </html>
