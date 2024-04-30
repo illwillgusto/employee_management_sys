29776597b0b356be902584fa331560ce8f4dccb5
Will Walton <will@endless3dge.com>
2024-04-30 18:08:34 -0500

    Update home.php
    jQuery and Bootstrap cdn scripts added

--- home.php
+++ home.php
@@ -17,6 +17,13 @@ $dbcon = $connection_object->connection('localhost', $dbname, $dbuser, $dbpass);
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Employee Management Dashboard</title>
+
+  <!-- jQuery library -->
+  <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+
+  <!-- Bootstrap compiled JS -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  
 </head>
 <body>
 
