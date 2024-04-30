c27201945ef152a1bc9941dd2c04a84e1542cb1d
Will Walton <will@endless3dge.com>
2024-04-30 18:10:18 -0500

    Update home.php
    id, data-spy, data-target, and data-offset attributes added to the html body tag

--- home.php
+++ home.php
@@ -23,9 +23,9 @@ $dbcon = $connection_object->connection('localhost', $dbname, $dbuser, $dbpass);
 
   <!-- Bootstrap compiled JS -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  
+
 </head>
-<body>
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="50">
 
 </body>
 </html>
