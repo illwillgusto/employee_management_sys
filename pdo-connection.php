315953eb581c1c94c08d7bf75809ed660635bdda
Will Walton <will@endless3dge.com>
2024-04-30 17:51:46 -0500

    Update pdo-connection.php
    PDO syntax corrected

--- pdo-connection.php
+++ pdo-connection.php
@@ -22,7 +22,7 @@
 
         //PDO Connection:
         $connect= new PDO("mysql:host=$hostname;dbname=$database", $username, $password, array(
-          POD::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+          PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
           PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
         ));
 
