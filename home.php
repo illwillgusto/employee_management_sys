14dd4ea37d59e61a4907a406e03b0a33a16ffc00
Will Walton <will@endless3dge.com>
2024-05-04 10:28:22 -0500

    Update home.php
    This SQL query retrieves all columns from the 'login_info' table where the 'Email' and 'Password' fields match the user-provided email and password stored in `$loginmail` and `$loginpass`, respectively. This query is used to verify if there exists a user record that matches both the email and the hashed password, effectively performing user authentication. Caution: Directly including variables in SQL queries like this can lead to SQL injection vulnerabilities. It is recommended to use prepared statements or parameterized queries to prevent such security risks.

--- home.php
+++ home.php
@@ -95,10 +95,13 @@ $dbcon = $connection_object->connection('localhost', $dbname, $dbuser, $dbpass);
 
 <?php
 
-// check if the 'login' button has been pressed on the form submission, it will retrieve the user's email and password 
+// check if the 'login' button has been pressed on the form submission, it will retrieve the user's email and password
 if (isset($_POST['login'])) {
   $loginmail= $_POST['loginEmail'];
   $loginpass= md5($_POST['loginPassword']);
+
+// query used ti verify if there exists a user record that matches both email and the hashed password 
+  $sql = " SELECT * FROM login_info WHERE Email='$loginmail' AND Password= '$loginpass' ";
 
 }
 ?>
